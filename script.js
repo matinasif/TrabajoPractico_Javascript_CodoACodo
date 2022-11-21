ceaee155b039fa910a3a87dee8222247049b18f2
Matias <nasif.matias@gmail.com>
2022-11-21 09:56:52 -0300

    eliminando codigo no utilizado

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-/*let ticketsResumen = document.querySelector("#ticketsResumen");
-ticketsResumen.addEventListener("click", ticketsPrice());
-
-let ticketsBorrar = document.querySelector("#ticketsBorrar");
-ticketsBorrar.addEventListener("click", borrarOutput);
-
-let ticketQuantity = document.querySelector("#ticketsQuantity");
-ticketQuantity.addEventListener("click", descuento);
-
-function ticketsPrice(evento){
-    let ticketQuantity = document.querySelector("#ticketsQuantity");
-    if (Number( ticketQuantity.value)){
-        let ticketCategory = document.querySelector("#inputState");
-        let totalPayment;
-        switch(ticketCategory.value){
-            case "estudiante":{
-                totalPayment = 200 * ticketQuantity.value *0.2;
-                break;
-            }
-            case "trainee":{
-                totalPayment = 200 * ticketQuantity.value *0.5;
-                break;
-            }
-            case "junior":{
-                totalPayment = 200 * ticketQuantity.value *0.85;
-                break;
-            }
-        }
-        document.querySelector("#ticketoutput").textContent = 'hola';
-    }
-}*/
-
 function totalAPagar(cantidadTickets, categoria){
     const valorTicket = 200;
     let total = cantidadTickets*valorTicket;
